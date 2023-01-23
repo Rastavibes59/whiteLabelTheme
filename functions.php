596e369d4e5c65a8d5f167eb90bacf5079f35f62
Rastavibes59 <louis.veyer@gmail.com>
2023-01-23 19:37:13 +0100

    Added ACF json load point

--- functions.php
+++ functions.php
@@ -174,6 +174,24 @@ function my_acf_json_save_point( $path ) {
     return $path;
     
 }
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+    
+    // remove original path (optional)
+    unset($paths[0]);
+    
+    
+    // append path
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    
+    
+    // return
+    return $paths;
+    
+}
+
 /* WP REMOVE ASSETS */
 
 //require_once(get_template_directory() . '/includes/functions/wp-remove-assets.php');

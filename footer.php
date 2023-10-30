666f0f08312115c6ea01b866069ce32d780f637c
Louis VEYER <louis.veyer@gmail.com>
2023-10-30 09:39:03 +0100

    FIX: display map

--- footer.php
+++ footer.php
@@ -13,8 +13,6 @@
                         tileSize: 512,
                         zoomOffset: -1,
                         accessToken: 'pk.eyJ1IjoicmFzdGF2aWJlczU5IiwiYSI6ImNsMmhldmttNTBjOXEzam56amszbXhrOTIifQ.VVfOEeHVviM5G1WlPGYllg'
-                    }).on('tileloadstart', function(event) {
-                        event.tile.setAttribute('loading', 'lazy');
                     }).addTo(map);
 
                     var pleinAirIcon = L.Icon.extend({
